98958753051f64494d1e0a02b4d76af0cea2c111
Seongju Hwang <ajanab153@naver.com>
2024-11-11 14:55:52 +0900

    CSV Parsing 처리
    CSV Parsing with ScriptableObjects

---
+++ BaseStruct.cs
@@ -0,0 +1,121 @@
+﻿using BaseEnum;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseStruct
+{
+    [System.Serializable]
+    public struct RewardStruct
+    {
+        public int Idx;
+        public int Amount;
+    }
+
+    #region 저장용 데이터 형태 
+
+    #region 인벤토리에 보유하는 타워들 
+    [System.Serializable]
+    public class TowerInvenData
+    {
+        List<TowerInvenSlot> towerSlotList = new();
+    }
+    [System.Serializable]
+    public class TowerInvenSlot
+    {
+        public int Id;
+        public int lv;
+        public int pieceCnt;
+    }
+    #endregion
+
+    #endregion
+
+    #region CSV 데이터 구조 형태 
+
+    // 챕터 데이터
+    [System.Serializable]
+    public class ChapterData
+    {
+        public int Id;
+        public string ChapterTitle;
+        public int[] StageIdxs;                 // csv에서 '/'를 기준으로 split한뒤 int로 변환하여 배열로 만든다
+        public string ChapterImgName;
+    }
+
+    // 스테이지 데이터
+    [System.Serializable]
+    public class StageData
+    {
+        public int Id;
+        public string StageName;
+        public int WaveIdx;
+        public RewardStruct[] rewardStructs;    // csv 에서 '|' 를 기준으로 split한뒤 각 배열을 다시 '/' 로 split 해서 RewardStruct 생성 
+        public string StageImgName;
+        public int NeedStamina;
+        public string SceneName;
+        public string LevelPrefabName;
+    }
+
+    // 웨이브 데이터
+    [System.Serializable]
+    public class WaveData
+    {
+        public int Id;
+        public int WaveIdx;
+        public int WaveType;
+        public int[] MonsterIdxs;           // csv에서 '/'를 기준으로 split 해서 int 변환 후 배열 생성
+        public float WaveTime;
+        public int SummonCnt;
+        public int MaxCount;
+        public float MultiplyHp;
+        public float MultiplySpeed;
+        public float MultiplyAtsp;
+    }
+
+    // 몬스터 데이터
+    [System.Serializable]
+    public class MonsterData
+    {
+        public int Id;                  // 몬스터 ID
+        public string Name;             // 몬스터 이름
+        public string PrefabName;       // 몬스터 프리팹 이름
+        public int IsBoss;              // MONSTER_TYPE;
+        public int HP;
+        public float Speed;             // 기본값이 되는 스피드 -> wave에 따라 값이 증감됨
+        public int Atk;
+        public int DropExp;             // 획득 경험치량
+        public float DropBeads;         // 경험치 구슬 드랍 확률 max = 100
+    }
+
+    // 테이블에서 읽어올 타워의 기본 데이터 
+    [System.Serializable]
+    public class TowerCSVData
+    {
+        public int Id;                  // 타워 ID
+        public string Name;             // 타워 이름
+        public string Desc;             // 타워 설명
+
+        public int IsAvailable;         // 타워 사용가능 여부 -> 실제로 획득할 수 있는 타워 ( 디버프 타워 제외 )
+
+        public string PrefabName;       // 타워 프리팹 이름
+        public string IconName;         // 타워 아이콘 이름
+        public int TileCnt;             // 타워가 차지하는 타일 갯수
+        public string TilePrefab;       // 타워가 차지되는 타일의 프리팹 이름 
+
+        public float AttackDmg;         // 타워 공격력 
+        public float AttackSpeed;       // 타워 공격속도
+        public float Range;             // 타워 공격 범위 
+
+        public float TowerValue_1;      // 타워 보유 능력 수치 1
+        public float TowerValue_2;      // 타워 보유 능력 수치 2
+        public float TowerValue_3;      // 타워 보유 능력 수치 3
+
+        public int Grade;               // 타워 등급 
+
+        public float LvUpAtt;           // 레벨이 오를때 상승할 공격력 증가 % 
+        public int MaxLv;               // 인벤토리에서 강화할 수 있는 타워의 최대레벨
+        public int MaxMergeLv;          // 인게임에서 타워의 머지할 수 있는 최대레벨  
+    }
+
+    #endregion
+}
---
+++ Singleton.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T _instance;
+    public static bool HasInstance => _instance != null;
+    public static T TryGetInstance() => HasInstance ? _instance : null;
+    public static T Current => _instance;
+
+    // DontDestroyOnLoad 적용 여부를 결정하는 플래그
+    protected virtual bool IsPersistent => false;
+
+    /// <summary>
+    /// 싱글톤 디자인 패턴
+    /// </summary>
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                // Unity 2023.1 이상에서는 FindAnyObjectByType<T>() 사용 권장
+                _instance = FindAnyObjectByType<T>();
+
+                if (_instance == null)
+                {
+                    Debug.Log("Instance Check");
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name + "_AutoCreated";
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// Awake에서 인스턴스를 초기화합니다. 만약 Awake를 override해서 사용해야 한다면 base.Awake()를 호출해야 합니다.
+    /// </summary>
+    protected virtual void Awake()
+    {
+        InitializeSingleton();
+    }
+    
+    /// <summary>
+    /// 싱글톤을 초기화합니다.
+    /// </summary>
+    protected virtual void InitializeSingleton()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        if (_instance == null)
+        {
+            _instance = this as T;
+
+            // IsPersistent가 true일 때만 DontDestroyOnLoad 적용
+            if (IsPersistent)
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+        else if (_instance != this)
+        {
+            // 이미 인스턴스가 존재하면 중복 생성 방지
+            Destroy(this.gameObject);
+        }
+    }
+}
